66f5cad3097c2e7d5efc81b1c99335df22222eda
Shuheng Liu <wish1104@icloud.com>
2020-12-23 23:01:51 +0800

    feat(favicon): add favicon placeholder

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
 import flask
 from flask import Flask
+from flask import send_from_directory
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Home"
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
